da01178929c42069302a587428f49cf3d913dfd2
Shipra Sinha <shipra.juet@gmail.com>
2016-08-10 18:47:42 +0000

    Introduced index.html

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.use(bodyParser.json())
 
 // Index route
 app.get('/', function (req, res) {
-    res.send('Hello world, I am a chat bot')
+res.sendfile('index.html');
 })
 
 // for Facebook verification
